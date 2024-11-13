9a944b4cacf7f08a8dcb665a60a10f19ea09d9aa
Zen Futral <zen.futral@gmail.com>
2024-11-13 12:11:14 -0700

    NetworkNuke.bat
    Added some lines from another similar script.

--- NetNuke.bat
+++ NetNuke.bat
@@ -3,18 +3,27 @@
 
 # This script automates a network reset process.
 
+netsh wlan show wlanreport
 
+ipconfig /release
 ipconfig /flushdns
 ipconfig /registerdns
-ipconfig /release
 ipconfig /renew
+
+set http_proxy=
+set https_proxy=
 
 netsh int ip reset
 netsh winsock reset catalog
+netsh advfirewall reset
 netsh int ipv4 reset
 netsh int ipv6 reset
+netsh wlan delete profile name=* i=*
 
 msdt.exe /id NetworkDiagnosticsNetworkAdapter
 msdt.exe /id NetworkDiagnosticsWeb
 
+echo THIS SYSTEM NEEDS TO RESTART TO FINISH THE NETWORK RESET
 pause
+
+shutdown /r
